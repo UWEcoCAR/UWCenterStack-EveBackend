5b9162b2695349b1e4d56d3981c1f9ef0bce7711
arushs <arushshankar@gmail.com>
2014-04-22 19:37:26 -0700

    Added schema models for data

---
+++ models.js/User.js
@@ -0,0 +1,60 @@
+var mongoose = require('mongoose'),
+    Trip = require('./Trip.js'),
+    DataPoint = require('./DataPoint.js');
+
+// create user schema
+var userSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId, 
+    firstName: {type:String, default: ""},
+    lastName: {type:String, default: ""}.
+    distance: Number,
+    MPGe: Number,
+    cost: Number,
+    dieselEnergy, Number,
+    electricEnergy, Number
+});
+
+// define custom methods
+
+/**
+ * Updates the user stats based off of child trips
+ * @param Closure callback
+ */
+userSchema.methods.calculate = function(callback) {
+    console.log('calculated user: '+this._id);
+    callback && callback();
+};
+
+/**
+ * Creates a new trip linked to the user
+ * @param object params
+ * @param Closure callback
+ */
+userSchema.methods.newTrip = function(params, callback) {
+    params.userId = this._id;
+    (new Trip(params)).save(callback);
+}
+
+// cascade delete and calculate
+userSchema.pre('remove', function(next) {
+    Trip.find({userId : this._id}).remove().exec();
+    DataPoint.find({userId : this._id}).remove().exec();
+    next();
+});
+userSchema.pre('calculate', function(next) {
+    Trip.find({userId : this._id}).stream()
+        .on('err', function(err) {
+            console.log(error);
+            next();
+        })
+        .on('data', function(doc) {
+            doc.calculate();
+        })
+        .on('close', function() {
+            next();
+        });
+});
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
