a0eed049bf6955f59868c2788b6801abbe5d5427
thans <toreh@d-173-250-202-122.dhcp4.washington.edu>
2014-05-01 16:26:29 -0700

    working version pulling from mongo

--- scripts/main.js
+++ scripts/main.js
@@ -53,93 +53,6 @@ function calcRoute() {
 }
 
 
-var data = [
-  {
-   location: { // eco car lab
-     lat: 47.654109,
-     long: -122.304331
-   },
-   efficiency: 10
-  },
-  {
-   location: {
-     lat: 47.654127,
-     long: -122.305130
-   },
-   efficiency: 10
- },
- {
-   location: {
-     lat: 47.654961,
-     long: -122.304653
-   },
-   efficiency: 10
- },
- {
-   location: {
-     lat: 47.654940,
-     long: -122.304642
-   },
-   efficiency: 10
- },
- {
-   location: {
-     lat: 47.655821,
-     long: -122.304556
-   },
-   efficiency: 10
- },
- {
-   location: {
-     lat: 47.656812,
-     long: -122.304814
-   },
-   efficiency: 10
- },
- {
-   location: {
-     lat: 47.657830,
-     long: -122.304578
-   },
-   efficiency: 10
- },
- {
-   location: {
-     lat: 47.658437,
-     long: -122.305543
-   },
-   efficiency: 10
- },
- {
-   location: {
-     lat: 47.659044,
-     long: -122.306445
-   },
-   efficiency: 10
- },
- {
-   location: {
-     lat: 47.659558,
-     long: -122.307249
-   },
-   efficiency: 10
- },
- {
-   location: {
-     lat: 47.659767,
-     long: -122.308376
-   },
-   efficiency: 10
- },
- {
-   location: {
-     lat: 47.659717,
-     long: -122.309288
-   },
-   efficiency: 10
- }];
-
-
 function addEfficiency() {
   // var routeCoordinates = [
   //   new google.maps.LatLng(47.654109, -122.304331), // eco car lab
@@ -155,98 +68,107 @@ function addEfficiency() {
   //   new google.maps.LatLng(47.659767, -122.308376),
   //   new google.maps.LatLng(47.659717, -122.309288) // stevens and 17th
   // ];
-  var routeCoordinates = [];
-  for (var i = 0; i < data.length; i++) {
-    routeCoordinates.push(new google.maps.LatLng(data[i].location.lat, data[i].location.long));
-  }
 
-  var to45th = [
-    new google.maps.LatLng(47.659717, -122.309288), // stevens and 17th
-    new google.maps.LatLng(47.660121, -122.309572),
-    new google.maps.LatLng(47.660728, -122.309577),
-    new google.maps.LatLng(47.661194, -122.309561),
-    new google.maps.LatLng(47.661263, -122.310221),
-    new google.maps.LatLng(47.661263, -122.311106),
-    new google.maps.LatLng(47.661270, -122.311879) // 45th and 15th
-  ];
+  $.ajax({
+    url: "/getDataPoints",
+    type: 'GET'
+  }).done(function(data) {
+    console.log("data returned" + data);
+    var routeCoordinates = [];
+    for (var i = 0; i < data.length; i++) {
+      routeCoordinates.push(new google.maps.LatLng(data[i].location.lat, data[i].location.long));
+    }
+    var firstPath = new google.maps.Polyline({
+      path: routeCoordinates,
+      geodesic: true,
+      strokeColor: calculateColor(data, 0, data.length),
+      strokeOpacity: 1.0,
+      strokeWeight: 3
+    });
+    firstPath.setMap(map);
 
-  var toGrant = [
-    new google.maps.LatLng(47.661270, -122.311879), // 45th and 15th
-    new google.maps.LatLng(47.661046, -122.311959),
-    new google.maps.LatLng(47.660721, -122.311970),
-    new google.maps.LatLng(47.660230, -122.312002),
-    new google.maps.LatLng(47.659666, -122.312034),
-    new google.maps.LatLng(47.659001, -122.312067),
-    new google.maps.LatLng(47.658503, -122.312088),
-    new google.maps.LatLng(47.657599, -122.312099),
-    new google.maps.LatLng(47.656898, -122.312120),
-    new google.maps.LatLng(47.656255, -122.312163),
-    new google.maps.LatLng(47.655836, -122.312152),
-    new google.maps.LatLng(47.655359, -122.312174) // 15th and Grant
-  ];
+    var to45th = [
+      new google.maps.LatLng(47.659717, -122.309288), // stevens and 17th
+      new google.maps.LatLng(47.660121, -122.309572),
+      new google.maps.LatLng(47.660728, -122.309577),
+      new google.maps.LatLng(47.661194, -122.309561),
+      new google.maps.LatLng(47.661263, -122.310221),
+      new google.maps.LatLng(47.661263, -122.311106),
+      new google.maps.LatLng(47.661270, -122.311879) // 45th and 15th
+    ];
 
-  var toLab = [
-    new google.maps.LatLng(47.655359, -122.312174), // 15th and Grant
-    new google.maps.LatLng(47.655200, -122.311315),
-    new google.maps.LatLng(47.654868, -122.310393),
-    new google.maps.LatLng(47.654116, -122.310446),
-    new google.maps.LatLng(47.653458, -122.310446),
-    new google.maps.LatLng(47.652895, -122.310264),
-    new google.maps.LatLng(47.652324, -122.309363),
-    new google.maps.LatLng(47.652042, -122.308172),
-    new google.maps.LatLng(47.652251, -122.306434),
-    new google.maps.LatLng(47.652757, -122.305651),
-    new google.maps.LatLng(47.653270, -122.305243),
-    new google.maps.LatLng(47.653834, -122.305157),
-    new google.maps.LatLng(47.654101, -122.305125) // entrance to lab
-  ];
+    var toGrant = [
+      new google.maps.LatLng(47.661270, -122.311879), // 45th and 15th
+      new google.maps.LatLng(47.661046, -122.311959),
+      new google.maps.LatLng(47.660721, -122.311970),
+      new google.maps.LatLng(47.660230, -122.312002),
+      new google.maps.LatLng(47.659666, -122.312034),
+      new google.maps.LatLng(47.659001, -122.312067),
+      new google.maps.LatLng(47.658503, -122.312088),
+      new google.maps.LatLng(47.657599, -122.312099),
+      new google.maps.LatLng(47.656898, -122.312120),
+      new google.maps.LatLng(47.656255, -122.312163),
+      new google.maps.LatLng(47.655836, -122.312152),
+      new google.maps.LatLng(47.655359, -122.312174) // 15th and Grant
+    ];
 
-  var firstPath = new google.maps.Polyline({
-    path: routeCoordinates,
-    geodesic: true,
-    strokeColor: calculateColor(data, 0, data.length),
-    strokeOpacity: 1.0,
-    strokeWeight: 3
+    var toLab = [
+      new google.maps.LatLng(47.655359, -122.312174), // 15th and Grant
+      new google.maps.LatLng(47.655200, -122.311315),
+      new google.maps.LatLng(47.654868, -122.310393),
+      new google.maps.LatLng(47.654116, -122.310446),
+      new google.maps.LatLng(47.653458, -122.310446),
+      new google.maps.LatLng(47.652895, -122.310264),
+      new google.maps.LatLng(47.652324, -122.309363),
+      new google.maps.LatLng(47.652042, -122.308172),
+      new google.maps.LatLng(47.652251, -122.306434),
+      new google.maps.LatLng(47.652757, -122.305651),
+      new google.maps.LatLng(47.653270, -122.305243),
+      new google.maps.LatLng(47.653834, -122.305157),
+      new google.maps.LatLng(47.654101, -122.305125) // entrance to lab
+    ];
+
+
+
+    var secondPath = new google.maps.Polyline({
+      path: to45th,
+      geodesic: true,
+      strokeColor: '#FFA500',
+      strokeOpacity: 1.0,
+      strokeWeight: 3
+    });
+
+    var thirdPath = new google.maps.Polyline({
+      path: toGrant,
+      geodesic: true,
+      strokeColor: '#008000',
+      strokeOpacity: 1.0,
+      strokeWeight: 3
+    });
+
+    var fourthPath = new google.maps.Polyline({
+      path: toLab,
+      geodesic: true,
+      strokeColor: '#FFFF00',
+      strokeOpacity: 1.0,
+      strokeWeight: 3
+    });
+
+
+    secondPath.setMap(map);
+    thirdPath.setMap(map);
+    fourthPath.setMap(map);
+
+
+    // zoom to start of the route
+    map.setZoom(14);
+    map.setCenter(routeCoordinates[0]);
+
+    var summaryPanel = $('#directions')[0];
+    summaryPanel.innerHTML = '';
+    summaryPanel.innerHTML += '<b>Driving around campus</b><br>';
+    summaryPanel.innerHTML += 'Starting at the EcoCAR lab and heading North!<br />';
   });
-
-  var secondPath = new google.maps.Polyline({
-    path: to45th,
-    geodesic: true,
-    strokeColor: '#FFA500',
-    strokeOpacity: 1.0,
-    strokeWeight: 3
-  });
-
-  var thirdPath = new google.maps.Polyline({
-    path: toGrant,
-    geodesic: true,
-    strokeColor: '#008000',
-    strokeOpacity: 1.0,
-    strokeWeight: 3
-  });
-
-  var fourthPath = new google.maps.Polyline({
-    path: toLab,
-    geodesic: true,
-    strokeColor: '#FFFF00',
-    strokeOpacity: 1.0,
-    strokeWeight: 3
-  });
-
-  firstPath.setMap(map);
-  secondPath.setMap(map);
-  thirdPath.setMap(map);
-  fourthPath.setMap(map);
-
-
-  // zoom to start of the route
-  map.setZoom(14);
-  map.setCenter(routeCoordinates[0]);
-
-  var summaryPanel = $('#directions')[0];
-  summaryPanel.innerHTML = '';
-  summaryPanel.innerHTML += '<b>Driving around campus</b><br>';
-  summaryPanel.innerHTML += 'Starting at the EcoCAR lab and heading North!<br />';
 }
 
 function calculateColor(data, low, high) {
--- server.js
+++ server.js
@@ -4,34 +4,199 @@ var http = require("http"),
     fs = require("fs")
     port = process.argv[2] || 8888;
 
-http.createServer(function(request, response) {
+var express = require("express");
+var app = express();
+var mongoose = require('mongoose');
 
-  var uri = url.parse(request.url).pathname
-    , filename = path.join(process.cwd(), uri);
+var User;
+var Trip;
+var DataPoint;
 
-  path.exists(filename, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
-      response.end();
-      return;
+app.get("/getDataPoints", function(req, res) {
+  console.log("request for data points");
+  databaseQueries.getDataPoints(function(data) {
+    console.log("I got the data", data);
+    var efficiencyAndLocation = [];
+    for (var i = 0; i < data.length; i++) {
+      efficiencyAndLocation.push({
+        location: data[i].location,
+        efficiency: Math.floor((Math.random() * 25) + 1)
+      });
     }
-
-    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-    fs.readFile(filename, "binary", function(err, file) {
-      if(err) {
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
-
-      response.writeHead(200);
-      response.write(file, "binary");
-      response.end();
-    });
+    res.send(efficiencyAndLocation);
   });
-}).listen(parseInt(port, 10));
+});
+
+
+/* serves main page */
+app.get("/", function(req, res) {
+  res.sendfile('index.html')
+});
+
+
+
+/* serves all the static files */
+app.get(/^(.+)$/, function(req, res){
+   console.log('static file request : ' + req.params);
+   res.sendfile( __dirname + req.params[0]);
+});
+
+var port = process.env.PORT || 8888;
+app.listen(port, function() {
+ console.log("Listening on " + port);
+});
+
+
+
+mongoose.connect('mongodb://localhost/ecocarData');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+var databaseQueries = {};
+
+db.once('open', function(callback) {
+    // ready
+    console.log("database ready");
+  var dataPointSchema = mongoose.Schema({
+      _id: mongoose.Schema.Types.ObjectId,
+      tripId: mongoose.Schema.Types.ObjectId,
+      location: {lat: Number, long: Number},
+      fuelConsumption: Number,
+      pedalPosition: Number,
+      optimalPosition: Number,
+      speed: Number,
+      acceleration: Number,
+      engineTRQ: Number,
+      engineRPM: Number,
+      motorTRQ: Number,
+      motorRPM: Number,
+      batterySoc: {type: Number, min: 0, max: 100},
+      batteryVolatage: Number,
+      batteryCurrent: Number,
+      fuel: {type: Number, min: 0, max: 100},
+      engineTemp: Number,
+      motorTemp: Number,
+      batteryTemp: Number,
+      elevation: Number,
+      timestamp: {type: Date, default: Date.now}
+  });
+
+  var tripSchema = mongoose.Schema({
+      userId: mongoose.Schema.Types.ObjectId,
+      _id: mongoose.Schema.Types.ObjectId,
+      startLocation: {
+          time: {type: Date, default: Date.now},
+          location: {lat: Number, long: Number},
+      },
+      endLocation: {
+          time: Date,
+          location: {lat: Number, long: Number},
+      },
+      MPGe: Number,
+      distance: Number,
+      cost: Number,
+      speed: Number,
+      dieselEnergy: Number,
+      electricEnergy: Number
+  });
+
+
+  // define custom methods
+  // Taken from nicks code
+
+  /**
+   * Updates the user stats based off of child trips
+   * @param Closure callback
+   */
+  tripSchema.methods.calculate = function(callback) {
+      console.log('calculated trip: '+this._id);
+      callback && callback(null, this);
+  };
+
+  /**
+   * Adds a DataPoint to the trip
+   * @param object params
+   * @param Closure callback
+   */
+  tripSchema.methods.newPoint = function(params, callback) {
+      params.tripId = this._id;
+      (new DataPoint(params)).save(callback);
+  };
+
+  // make remove and calculate cascade down
+  tripSchema.pre('remove', function(next) {
+      DataPoint.find({tripId : this._id}).remove(next);
+  });
+  tripSchema.pre('calculate', function(next) {
+      // not cascading this yet, might not even need too
+      next();
+  });
+
+// create user schema
+var userSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    firstName: {type:String, default: ""},
+    lastName: {type:String, default: ""},
+    distance: Number,
+    MPGe: Number,
+    cost: Number,
+    dieselEnergy: Number,
+    electricEnergy: Number
+});
+
+// define custom methods
+
+/**
+ * Updates the user stats based off of child trips
+ * @param Closure callback
+ */
+userSchema.methods.calculate = function(callback) {
+    console.log('calculated user: '+this._id);
+    callback && callback();
+};
+
+/**
+ * Creates a new trip linked to the user
+ * @param object params
+ * @param Closure callback
+ */
+userSchema.methods.newTrip = function(params, callback) {
+    params.userId = this._id;
+    (new Trip(params)).save(callback);
+}
+
+// cascade delete and calculate
+userSchema.pre('remove', function(next) {
+    Trip.find({userId : this._id}).remove().exec();
+    DataPoint.find({userId : this._id}).remove().exec();
+    next();
+});
+userSchema.pre('calculate', function(next) {
+    Trip.find({userId : this._id}).stream()
+        .on('err', function(err) {
+            console.log(error);
+            next();
+        })
+        .on('data', function(doc) {
+            doc.calculate();
+        })
+        .on('close', function() {
+            next();
+        });
+});
+
+ User = mongoose.model('User', userSchema, 'User');
+ Trip = mongoose.model('Trip', tripSchema, 'Trip');
+ DataPoint = mongoose.model('DataPoint', dataPointSchema, 'DataPoint');
+    databaseQueries.getDataPoints = function(callback) {
+      return DataPoint.find(function(err, dataPts) {
+        if (err) return console.log(err);
+        console.log(dataPts);
+        callback(dataPts);
+      });
+    }
+});
+
+
+
 
 console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
